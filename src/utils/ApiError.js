3de3a0fe1ccecfde585fb4267446938b40aabc5d
msaadnanil18 <140881151+msaadnanil18@users.noreply.github.com>
2024-01-07 22:00:51 +0000

    add utils code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "something went wrong",
+        error = [],
+        statck = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors 
+        if (statck) {
+           this.stack = statck 
+        } else{
+            error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
