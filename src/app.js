a9f7adc14d2b6c1157155235893c4ce8fb761492
msaadnanil18 <140881151+msaadnanil18@users.noreply.github.com>
2024-01-07 20:41:53 +0000

    connect server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import  express, { urlencoded }  from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express()
+app.use(cors({
+    origin: process.env.CROS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({
+    limit: "16kb"
+}))
+app.use(urlencoded({
+    limit:"16kb",
+    extended:true
+}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
+ 
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import { Collection } from "mongoose";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+import { app } from "./app.js";
+
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(() => {
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running: ${process.env.PORT}`)
+})
+})
+.catch((error) => {
+    console.log("MONGO db connection error", error )
+})
