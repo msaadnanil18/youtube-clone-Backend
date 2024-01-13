d9407fdbfbd5b552bebe3fefb778ce5132117778
msaadnanil18 <140881151+msaadnanil18@users.noreply.github.com>
2024-01-13 08:38:46 +0000

    file upload setup

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -22,9 +22,7 @@ const uploadCloudnary = async (localFilePath) => {
   }
 }
 
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+export {uploadCloudnary}
 
 
   
