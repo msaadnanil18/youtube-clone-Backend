66a96908e36a5cb2bd357b9448cd50de0ef698a4
msaadnanil18 <msaadnanil18.con>
2024-01-15 02:25:00 +0530

    working on register user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,28 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 
 const registerUser = asyncHandler(async(req, res) => {
-  res.status(200).json({
-    message:"ok"
+  // res.status(200).json({
+  //   message:"ok"
+  // })
+
+  const {username, email, password, name } = req.body
+  console.log("email", email)
+  if (
+    [fullName, email, username, password, ].some((field) => field?.trim() === "" )
+  ) {
+    throw new ApiError(400, "all fields are required")
+  }
+
+  const exitedUser = User.findOne({
+    $or:[{username}, {email}, ]
   })
+  
+  if (exitedUser) {
+    throw new ApiError(409, "you are all ready exited")
+  }
+
 })
 
 export {registerUser}
