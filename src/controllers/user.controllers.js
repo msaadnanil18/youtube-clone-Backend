9733e937a5c8ccbe8c8a09c2d85a57aacaf62215
msaadnanil18 <msaadnanil18.con>
2024-01-22 02:46:42 +0530

    add user controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
+import {uploadCloudnary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async(req, res) => {
   // res.status(200).json({
-  //   message:"ok"
+  //   message:"baba yaga"
   // })
 
   const {username, email, password, name } = req.body
@@ -23,6 +25,38 @@ const registerUser = asyncHandler(async(req, res) => {
     throw new ApiError(409, "you are all ready exited")
   }
 
+  const avatarOnLocalPath = req.files?.avatar[0]?.path
+  const coverImageLocalPath =  req.files?.coverImage[0].path
+
+  if (!avatarOnLocalPath) {
+    throw new ApiError(400, "Avatar file is required")
+    
+  }
+const avatar = await uploadCloudnary(avatarOnLocalPath)
+ const coverImage  = await uploadCloudnary(coverImageLocalPath)
+
+ if (!avatar) {
+  throw new ApiError(400, "Avatar file is required")
+ }
+  const user =  await User.create({
+      fullName,
+      avatar:avatar.url,
+      coverImage: coverImage?.url || "" ,
+      email,
+      password,
+      username: username.toLowerCase()
+    })
+  const createdUseer = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+
+  if (!createdUseer) {
+    throw new ApiError(500, "something went wrong while register the user")
+  }
+
+   return status(201).json(
+    new ApiResponse(200, createdUseer, "user registered successfully ")
+   )
 })
 
 export {registerUser}
